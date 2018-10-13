14926311f814230995de5781825714ee37bc1459
Roy A Vanegas <roy@thecodeeducators.com>
2018-10-13 16:58:54 -0400

    Add example of appending an element to the <body>

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 window.onload = () => {
     'use strict';
 
-    alert(`hello`);
+    // Retrieve a reference to the <body> element, assigning it to the body variable.
+    let body = document.getElementsByTagName(`body`)[0];
+
+    // Create a new <p> element.
+    let p = document.createElement(`p`);
+
+    // Create a text node.
+    let content = document.createTextNode(`hello, world`);
+
+    p.appendChild(content);
+    body.appendChild(p);
 };
